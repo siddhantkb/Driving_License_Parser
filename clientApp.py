fb1abc85d18eb64abc59c483ffd0b0c180574b1f
siddhantbk <siddhantbhagat46@gmail.com>
2023-04-02 21:26:03 +0530

    global

--- clientApp.py
+++ clientApp.py
@@ -71,5 +71,4 @@ def predictRoute():
 #port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     clApp = ClientApp()
-    #app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=7000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
